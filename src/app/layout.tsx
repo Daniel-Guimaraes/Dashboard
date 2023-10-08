04f144b00128c4c57a27e2ca4caae5ff709609f8
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-10-08 16:01:13 -0300

    Responsive sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className={`${inter.className} font-sans`}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
-          <main className="px-4 pb-12 pt-8">{children}</main>
+          <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
+            {children}
+          </main>
         </div>
       </body>
     </html>
